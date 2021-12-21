ce0ce125c93053a1cb9f55083cc169e4bf74765d
Shivani R <2001.srkmaysrk@gmail.com>
2021-12-21 22:12:15 +0530

    made dice roller

--- app/src/main/java/com/sample/happybirthday/MainActivity.kt
+++ app/src/main/java/com/sample/happybirthday/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.sample.happybirthday
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton = findViewById<Button>(R.id.button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Rolled!", Toast.LENGTH_SHORT).show()
+            val result = findViewById<TextView>(R.id.textView)
+            result.text = rollDice().toString()
+        }
+    }
+
+    fun rollDice(): Int {
+        return (1..6).random()
     }
 }
