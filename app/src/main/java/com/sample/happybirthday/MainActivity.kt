f439105447c66035880576822b4ee80602809284
Shivani R <2001.srkmaysrk@gmail.com>
2021-12-21 22:39:27 +0530

    added images to dice roller

--- app/src/main/java/com/sample/happybirthday/MainActivity.kt
+++ app/src/main/java/com/sample/happybirthday/MainActivity.kt
@@ -2,7 +2,7 @@ package com.sample.happybirthday
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,14 +11,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton = findViewById<Button>(R.id.button)
+        val result = findViewById<ImageView>(R.id.imageView)
         rollButton.setOnClickListener {
             Toast.makeText(this, "Rolled!", Toast.LENGTH_SHORT).show()
-            val result = findViewById<TextView>(R.id.textView)
-            result.text = rollDice().toString()
+            val dice = rollDice()
+            when (dice) {
+                1 -> result.setImageResource(R.drawable.dice_1)
+                2 -> result.setImageResource(R.drawable.dice_2)
+                3 -> result.setImageResource(R.drawable.dice_3)
+                4 -> result.setImageResource(R.drawable.dice_4)
+                5 -> result.setImageResource(R.drawable.dice_5)
+                6 -> result.setImageResource(R.drawable.dice_6)
+            }
         }
     }
 
-    fun rollDice(): Int {
+    private fun rollDice(): Int {
         return (1..6).random()
     }
 }
